cba313a080fafb5ddd62f038b045d7afac47138d
Artem Viliutich <viliutich.artem@gmail.com>
2020-09-29 22:26:00 +0700

    go example work with function

--- golangTop.go
+++ golangTop.go
@@ -2,15 +2,15 @@ package main
 
 import "fmt"
 
+func calc(x, y int) (out1, out2 int) {
+	out1 = x + y
+	out2 = x - y
+	return
+}
 func main() {
 	num1 := 5
 	num2 := 4
 
-	result := add(num1, num2)
-	fmt.Println(result)
-}
-
-func add(x int, y int) int {
-	var out = x + y
-	return out
+	result1, resullt2 := calc(num1, num2)
+	fmt.Println(result1, resullt2)
 }
